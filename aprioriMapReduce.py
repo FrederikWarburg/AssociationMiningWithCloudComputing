e6cabd3ec21aa0dc5aaaf85869b3fcffc76f0911
Dwimawesome <frederikboehyttel@gmail.com>
2018-11-14 20:37:32 +0100

    implemented some of the stuff from david. It doesn't count up the support thogh

--- aprioriMapReduce.py
+++ aprioriMapReduce.py
@@ -1,17 +1,64 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 import numpy as np
+import itertools
+
+
 class aprioriMapReduce(MRJob):
     #
-    def mapper(self, _, text_file):
+    def freq_mapper(self, _, text_file):
         text_file = text_file.split(",")
         user = text_file[0]
+        items = text_file[1:]
+        itemsets = itertools.combinations(items, 2)
+        for p in itemsets:
+            yield p, 1
 
-        for i in range(1,len(text_file)):
-            yield text_file[i], 1
+    def fp_reducer(self, page, value):
+        support = sum(value)
+        yield page, support
 
-    def reducer(self, page, value):
+    def fp_to_association_map(self, page, support):
+        yield page, support  # Yield the full pattern
+        if len(page) > 1:    # Yield every subpattern
+            for item in page:
+                others = [i for i in page if i != item]
+                yield others, list((page,support))
 
-        yield page, sum(value)
+    def fp_to_association_reduce(self, left, values):
+        # Find value that is not a pattern-support pair
+        #print(left, list(values))
+        rule = {}
+        tempt = list(values)
+        fullpattern_support = []
+        for sets in tempt:
+            if isinstance(sets, int):
+                tempt.remove(sets)
+                continue
+            fullpattern_support = 5
 
+        for value in tempt:
+            pattern = value[0]
+            support = value[1]
+            #break
+            #pattern, support = value.split(':')
+            #for page in sets:
+            #    left.remove(page)
+            confidence = float(support)/float(fullpattern_support)
+
+            #yield left, pattern, support, confidence
+            #yield left, pattern
+            rule = { 'left': left, 'right': pattern, 
+                    'sup': support, 'conf': confidence }
+            print(rule)        
+
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.freq_mapper,
+                   reducer = self.fp_reducer),
+            MRStep(mapper = self.fp_to_association_map,
+                reducer = self.fp_to_association_reduce)
+        ]
 if __name__ == '__main__':
     aprioriMapReduce.run()
