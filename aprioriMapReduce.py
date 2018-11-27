c6c50125845c3239d31ef7ec690740eef8c37d46
dwimawesome <frederikboehyttel@gmail.com>
2018-11-27 16:25:37 +0100

    selv tak Frede

--- aprioriMapReduce.py
+++ aprioriMapReduce.py
@@ -1,19 +1,22 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import itertools
+import sys
 
 # Global variables
 SIZE_OF_FREQ_ITEM_SET = 2
-MINIMUM_CONFIDENCE = 0.05
-MINIMUM_SUPPORT = 1
+MINIMUM_CONFIDENCE = 0.5
 
 class aprioriMapReduce(MRJob):
 
     # First mapper that generates up to SIZE_OF_FREQ_ITEM_SET itemset.
     def freq_mapper(self, _, text_file):
-        text_file = text_file.split(",")
+        text_file = text_file.replace('"', '').replace(" ", "").split(",")
         items = text_file[1:]
         # generate the number of itemsets
+        for item in items:
+            yield [item], 1
+
         itemsets = itertools.combinations(items, SIZE_OF_FREQ_ITEM_SET)
         for p in itemsets:
             yield p, 1
@@ -29,9 +32,10 @@ class aprioriMapReduce(MRJob):
         if len(page) > 1:    # Yield every subpattern in the page.
             for item in page:
                 others = [i for i in page if i != item]
+                #print(others, (page,support))
                 yield others, (page, support)
         else:
-            yield page, support
+            yield page, [support]
 
     # Combine the itemsets, so that each left value is described by the rest of the values.
     def subset_reducer(self, left, values):
@@ -40,22 +44,24 @@ class aprioriMapReduce(MRJob):
         # calculate full support by going through the list of values
         full_support = 0
         for value in tempt:
-            full_support += value[1]
+            if isinstance(value[0], int):
+                full_support = value[0]
+                tempt.remove(value)
 
-        if full_support < MINIMUM_SUPPORT:
-            # Go through each pattern and their corrosponding subpaterns.
-            for i in range(len(tempt)):
-                right_side = ""
-                for item in tempt[i][0]:
-                    if item != left_side:
-                        right_side += item
-                support = tempt[i][1]
-                confidence = support / full_support
-                # Print rules if they have high confidence and above minimum support.
-                if confidence > MINIMUM_CONFIDENCE and support > MINIMUM_SUPPORT:
-                    rule = { 'left': left_side, 'right': right_side,
-                            'sup': support, 'conf': confidence}
-                    print(rule)
+        # Go through each pattern and their corrosponding subpaterns.
+        for i in range(len(tempt)):
+            right_side = ""
+            for item in tempt[i][0]:
+                if item != left_side:
+                    right_side += item
+            support = tempt[i][1]
+            confidence = support / full_support
+            # Print rules if they have high confidence and above minimum support.
+            if confidence > MINIMUM_CONFIDENCE:
+                rule = { 'left': left_side, 'right': right_side,
+                    'sup': support, 'conf': confidence}
+                print(rule)
+
 
     # Define the steps
     def steps(self):
