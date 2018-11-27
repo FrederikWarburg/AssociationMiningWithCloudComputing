5bde601472883234a5f1019e163fe3a9cb69e6c4
dwimawesome <frederikboehyttel@gmail.com>
2018-11-27 14:43:23 +0100

    added 5k and changes to batch script

--- script.sh
+++ script.sh
@@ -2,11 +2,16 @@
 echo "#################"
 echo "Start of program"
 echo "################"
-python3 WordFrequency.py Data/3mil.csv > userdata.txt
+
+mkdir tmp
+python3 WordFrequency.py Data/3mil.csv > tmp/userdata.txt
+
 echo "###################"
 echo "Finished 1'st step"
 echo "###################"
-python3 aprioriMapReduce.py userdata.txt > rules.txt
+
+python3 aprioriMapReduce.py tmp/userdata.txt > tmp/rules.txt
+
 echo "################################"
 echo "Rules can be found in rules.txt"
 echo "################################"
