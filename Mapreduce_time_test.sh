0da5e3a1c992d4be7cd7adefcc5a5e337436c971
dwimawesome <frederikboehyttel@gmail.com>
2018-12-04 21:16:57 +0100

    small changes and removal

--- Mapreduce_time_test.sh
+++ Mapreduce_time_test.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
+echo "#################"
 echo "Start of program"
-mkdir tempt
-python3 WordFrequency.py data/"$@".csv > tempt/"$@".txt
-echo "Finished first part"
+echo "################"
 mkdir mapreduce_results
-python3 aprioriMapReduce.py tempt/"$@".txt > mapreduce_results/"$@".txt
-echo "all done - rules can be found in mapreduce_results folder"
+python3 aprioriMapReduce.py data/5k.txt
+echo "#################"
+echo "Done 5"
+echo "################################"
+python3 aprioriMapReduce.py data/10k.txt
+echo "#################"
+echo "Done 10 "
+echo "################################"
+python3 aprioriMapReduce.py data/25k.txt
+echo "#################"
+echo "Done 25 "
+echo "################################"
+python3 aprioriMapReduce.py data/50k.txt
+echo "#################"
+echo "Done 50"
+echo "################################"
+python3 aprioriMapReduce.py data/100k.txt
+echo "#################"
+echo "Done 100"
+echo "################################"
+python3 aprioriMapReduce.py data/350k.txt
+echo "################################"
+echo "Rules can be found in rules.txt"
+echo "################################"
